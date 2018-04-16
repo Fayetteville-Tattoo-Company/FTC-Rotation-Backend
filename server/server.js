cb76c7395ebb7fa5596424f31d110cd15113685d
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-04-16 01:25:25 -0400

    mongo config

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const server = require('./');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:7777/test'
 mongoose.Promise = global.Promise;
-const connect = mongoose.connect(`mongodb://<dbuser>:<dbpassword>@ds143099.mlab.com:43099/heroku_6v7tqx6q`);
+const connect = mongoose.connect(mongoURI);
 connect.then(() => {
   console.log("MongoDB Server Running : 7777");
+  server.listen(port, () => console.log(`Main Server Running : ${port}`));
 })
 .catch((err) => console.log("ERROR STARTING SERVERS", err));
-server.listen(port, () => console.log(`Main Server Running : ${port}`));
 
