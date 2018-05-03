e76a8186da57023453467231e026fc067dea9566
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-05-03 11:29:48 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const key = process.env.KEY;
 
 
 server.use(cors({
-  origin: process.env.SERVER,
+  origin: "*",
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
   optionsSuccessStatus: 200 
 }));
