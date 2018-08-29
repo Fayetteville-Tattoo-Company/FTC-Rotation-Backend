6c5f53dffe18034b0738633db7aa18bddc342a23
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-08-28 21:22:38 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const server = express();
-const jwt = require('json-web-token');
 const path = require('path');
 const key = process.env.KEY;
 
